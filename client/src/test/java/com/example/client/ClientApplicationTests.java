f202edcb747deac1f5bafdd487562181d5f2c063
salwaalmastouur <masalwa16@gmail.com>
2023-12-28 21:53:34 +0100

    first commit

---
+++ client/src/test/java/com/example/client/ClientApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
